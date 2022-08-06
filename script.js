9bdd48aa900d78b03b86bc70ba817555db5f28f5
awais-124 <72652913+awais-124@users.noreply.github.com>
2022-08-06 08:51:34 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+'use strict';
+const score0 = document.querySelector('#score--0');
+const score1 = document.querySelector('#score--1');
+const current0 = document.getElementById('current--0');
+const current1 = document.getElementById('current--1');
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
+const btnHold = document.querySelector('.btn--hold');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const dice = document.querySelector('.dice');
+let scores, currentScore, activePlayer, playing;
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0.classList.toggle('player--active');
+  player1.classList.toggle('player--active');
+};
+const initializingGame = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  score0.textContent = 0;
+  score1.textContent = 0;
+  current0.textContent = 0;
+  current1.textContent = 0;
+  dice.classList.add('hidden');
+  player0.classList.remove('player--winner');
+  player1.classList.remove('player--winner');
+  player0.classList.add('player--active');
+  player1.classList.remove('player--active');
+};
+initializingGame();
+
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    const diceRoll = Math.floor(Math.random() * 6) + 1;
+
+    dice.classList.remove('hidden');
+    dice.src = `dice-${diceRoll}.png`;
+
+    if (diceRoll !== 1) {
+      currentScore += diceRoll;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
+});
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    if (scores[activePlayer] >= 50) {
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      switchPlayer();
+    }
+  }
+});
+btnNew.addEventListener('click', function () {
+  if (!playing) {
+    initializingGame();
+  } else {
+    alert('🤷🤷🤷🤷🤷🤷🤷');
+  }
+});
